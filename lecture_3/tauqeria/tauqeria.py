041c468bd35e45f0155ac19f52faf12c059493cd
zaynbakhtiarx <zaynbakhtiarx@gmail.com>
2026-08-04 16:45:22 +0500

    Committing again

--- lecture_3/tauqeria/tauqeria.py
+++ lecture_3/tauqeria/tauqeria.py
@@ -12,16 +12,18 @@ def main():
     }
     get_item(items)
 
-
 def get_item(items):
     total = 0
+
     while True:
             try:
                 order = input("Item: ").title()
+
                 if order in items:
                     value = items[order]
                     total += value
                     print(f'Total: ${total:.2f}')
+
             except EOFError:
                  print()
                  break
